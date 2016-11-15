1c4864a9fc75cbd7d87fe1264c9f41360d71992c
arun <arunmscit86@gmail.com>
2016-11-15 18:05:33 +0530

    route commit

--- source/app/Http/routes.php
+++ source/app/Http/routes.php
@@ -61,7 +61,7 @@ $app->delete('api/v1/customer/{id}','ServiceappointmentController@deleteCustomer
 
 //$app->post('api/v1/appointment/service/{participants}/{branch1_id}/{service1_id}/{staff1_id}/{start_date}/{timezone_id}/{meetingtype_id}','ServiceappointmentController@getMatrix1_Result');
 
-$app->post('api/v1/appointment/service/','ServiceappointmentController@getMatrix1_Result');
+$app->post('api/v1/appointment/service','ServiceappointmentController@getMatrix1_Result');
 
 $app->post('api/v1/appointment/serviceRecurring/{participants}/{branch1_id}/{service1_id}/{staff1_id}/{start_date}/{end_date}/{repetition_type}/{timezone_id}/{meetingtype_id}','ServiceappointmentController@getMatrix2_Result');
 
